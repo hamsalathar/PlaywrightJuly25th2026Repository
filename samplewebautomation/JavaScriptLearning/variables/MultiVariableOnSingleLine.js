ad82baa1b6fd5484898adc5814e3169a374216f1
hamsalathar <r.hamsa2015@gmail.com>
2026-07-31 16:20:10 +0530

    Added HTML and JavaScript learning examples

---
+++ samplewebautomation/JavaScriptLearning/variables/MultiVariableOnSingleLine.js
@@ -0,0 +1,17 @@
+var a,b,c;
+a=10;
+b=20;
+c=30
+console.log(a,b,c);
+console.log("------------------------------------");
+
+let x,y,z;
+x=100;
+y=200;
+z=300;
+console.log(x,y,z);
+console.log("------------------------------------");
+
+const p="welcome", q="to", r="javascript";
+console.log(p,q,r);
+
---
+++ samplewebautomation/JavaScriptLearning/variables/const.js
@@ -0,0 +1,5 @@
+//declare varibale using const keyword.
+const z=100;
+console.log(z);
+const z=200;
+console.log(z);
